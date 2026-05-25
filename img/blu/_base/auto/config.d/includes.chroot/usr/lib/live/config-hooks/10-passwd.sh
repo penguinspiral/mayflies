434757839bc17d6f41a2058b555e319c85d47b99
penguinspiral <myles@linux.com>
2026-05-25 03:30:27 -0700

    feat(lb_config): Add live-config(7) live user passwd hook
    The live-config(7) hardcodes the live account user password at
    '/lib/live/config/0030-{user-setup,live-debconfig_passwd}' to 'live':
    
    ```
    _PASSWORD="8Ab05sVQ4LLps"
    ...
    live-debconfig live-debconfig/passwd/user-password-crypted string ${_PASSWORD}
    user-setup passwd/user-password-crypted string ${_PASSWORD}
    ```
    
    live-config(7) hook script(s) located under '/lib/live/hooks/*' (now
    '/usr/lib/live/hooks/*' due to 'usr-merge') are invoked by
    '/lib/live/config/9990-hooks' as 'LIVE_HOOKS=filesystem' configuration
    variable is set per: fb9f2c6e64b9954938ceeb156e26335b5be09998.
    Ref: https://manpages.debian.org/unstable/live-config-doc/live-config.7.en.html#live~24
    
    Commit includes outlining additional dependency to the live-config(7)
    configuration file based on the 'LIVE_USERNAME' value.

---
+++ img/blu/_base/auto/config.d/includes.chroot/usr/lib/live/config-hooks/10-passwd.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# The 'live-config' package does not expose an interface for setting the live user password
+# Authors of the Debian Live project expect the use of a boot-time hook:
+# https://live-team.pages.debian.net/live-manual/html/live-manual/customizing-run-time-behaviours.en.html
+
+# Password creation via mkpasswd(1) (included with 'whois' package)
+# Create password for chpasswd(8) ingestion (/etc/shadow)
+chpasswd --encrypted <<< 'debian:$y$j9T$ZQOFkgZCebsmdxMWJXLbs0$E/ZJ4qzKa19817qDMUM9KpKtYfG08QZbsXOBAs2XoF0'
