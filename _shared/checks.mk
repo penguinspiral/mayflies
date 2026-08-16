d7ec57ec79e65a915c5713ac06fdee4b40e34c16
penguinspiral <myles@linux.com>
2026-08-16 12:55:08 -0700

    fix(makefile): Use wildcard for file existence check
    Using make(1)'s wildcard function to determine file existence by means
    of pattern matching. Simplifies implementation by leveraging an
    in-built function from make(1).

--- _shared/checks.mk
+++ _shared/checks.mk
@@ -16,5 +16,5 @@ check_path = \
     $(strip $(foreach 1,$1, \
         $(call __check_path,$1,$(strip $(value 2)))))
 __check_path = \
-    $(if $(shell [ -e $1 ]),, \
+    $(if $(wildcard $1),, \
       $(error Missing file: $1$(if $2, ($2))))
